ac5055e074b718853a300459fa9d73680c102c3b
lethalgem <iwc123@gmail.com>
2024-01-04 20:12:03 -0600

    Add todo

--- src/handlers.rs
+++ src/handlers.rs
@@ -8,4 +8,5 @@ pub async fn auth_with_strava(mut req: Request) -> Result<String, GeneralError> 
     let body = req.json::<StravaAuthCode>().await?;
     let code = body.code;
     Ok(code)
+    /* TODO: take code, send to strava, receive response, dump needed data to database -- then on to retrieving activities! */
 }
